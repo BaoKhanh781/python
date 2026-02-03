19a504dc8697e29ca86e738d5d4a82d4c6805d91
BaoKhanh781 <152123666+BaoKhanh781@users.noreply.github.com>
2026-02-03 08:19:37 -0800

    Create try-except.py

---
+++ try-except.py
@@ -0,0 +1,17 @@
+def main():
+    print("Starting The Code Challenge")
+
+
+    print("Starting a Number Of Code Challenge")
+    # Enter Your Code Here:
+    my_number = input("Enter Your Number Here:")
+    try:
+        my_number = int(my_number)
+        print(my_number, 'is a number')
+    except:
+        print('please enter a number')
+        print(my_number, 'is not a number')
+    print("Ending My Number Of Code Challenge")
+    print("Ending The Code Challenge")
+
+main()
