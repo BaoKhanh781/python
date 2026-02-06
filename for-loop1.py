31381437982f366797506e1781baadc302903047
BaoKhanh781 <152123666+BaoKhanh781@users.noreply.github.com>
2026-02-06 07:36:59 -0800

    Create for-loop1.py

---
+++ for-loop1.py
@@ -0,0 +1,19 @@
+def main():
+  print("Starting The Code Challenge")
+  # Declare Any main() Variables - (Not Global)
+
+  print("Starting <Specific Part> Of Code Challenge")
+  # Enter Your Code Here:
+  print('Enter Your Code Here:')
+  print('This is a beautiful math list of numbers')
+  x = 100
+  for x in range(100):
+    print(x)
+    print(x*x)
+    print(x*x*x)
+    print("HORRAYYYY")
+ 
+  print("Ending for loop Of Code Challenge")
+  print("Ending The Code Challenge")
+
+main()
